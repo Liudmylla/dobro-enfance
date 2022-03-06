32dce57eeb858b0ec5b9ca870a198ba6a9f60016
Liudmylla <liudmyla12@yahoo.fr>
2022-03-06 09:53:56 +0100

    style photos

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ export default function App() {
     <Container>
       {/* <Header /> */}
       <ImageList>
-        <ImageListItem key="Subheader" cols={3}>
+        <ImageListItem key="Subheader" cols={2}>
           <ListSubheader component="div" style={{ textAlign: "center" }}>
             {" "}
             <img
@@ -54,20 +54,16 @@ export default function App() {
 
 const itemData = [
   {
-    img: "https://i.ibb.co/qMhN3qc/Whats-App-Image-2022-03-03-at-09-46-04-1.jpg",
-    title: "Confit de canard",
-    author: "Тушенка",
-  },
-  {
-    img: "https://i.ibb.co/LS1QM1y/Whats-App-Image-2022-03-04-at-18-06-48.jpg",
-    title: "Le camion est partis pour la frontière polonaise",
-    author: "Грузовик выехал к польской границе",
+    img: "https://i.ibb.co/sgCYFmR/Whats-App-Image-2022-03-04-at-17-38-48.jpg",
+    title: "Les bénévoles de l'association Dobro Enfance",
+    author: "Волонтеры ассоциации  Dobro Enfance ",
   },
   {
     img: "https://i.ibb.co/RTwf9vv/remerciement.jpg",
-    title: "Un camion 20m3 préparé par les volontaires ",
+    title: "Un camion 20m3 préparé par les bénévoles ",
     author: " et payé par la mairie de Bayonne est parti vendredi 04.03.22",
   },
+
   {
     img: "https://i.ibb.co/dfwcgP5/c4705daf-c02d-41bc-88bb-59bfe1f7b26c.jpg",
     title: "Aide pour les enfants ukrainiens",
@@ -75,8 +71,8 @@ const itemData = [
   },
   // {
   //   img: "https://i.ibb.co/dp4mVJT/1e90f60f-07d8-4208-b6c4-60dcecc38bb6.jpg",
-  //   title: "Le camion est pret à partir",
-  //   author: "Грузовик загружен и готова к выезду",
+  //   title: "Dons en soutien aux populations ukrainiennes",
+  //   author: "Помощь украинскому народу",
   //   rows: 2,
   //   cols: 2,
   //   featured: true,
@@ -86,20 +82,22 @@ const itemData = [
     title: "La mobilisation de gens était sans précédent",
     author: "Реакция людей была беспрецедентной",
   },
+  {
+    img: "https://i.ibb.co/dp4mVJT/1e90f60f-07d8-4208-b6c4-60dcecc38bb6.jpg",
+    title: "Le camion est pret à partir",
+    author: "Грузовик загружен и готова к выезду",
+  },
 
   {
-    img: "https://i.ibb.co/sgCYFmR/Whats-App-Image-2022-03-04-at-17-38-48.jpg",
-    title: "Les bénévoles",
-    author: "Волонтеры",
+    img: "https://i.ibb.co/LS1QM1y/Whats-App-Image-2022-03-04-at-18-06-48.jpg",
+    title: "Le camion est partis pour la frontière polonaise",
+    author: "Грузовик выехал к польской границе",
   },
 
   // {
-  //   img: "https://i.ibb.co/wdSmwDt/98c2cf77-d2a6-49c7-83cf-6b464b748908.jpg",
-  //   title: "Dons en soutien aux populations ukrainiennes",
-  //   author: "Помощь украинскому народу",
-  //   rows: 2,
-  //   cols: 2,
-  //   featured: true,
+  //   img: "https://i.ibb.co/qMhN3qc/Whats-App-Image-2022-03-03-at-09-46-04-1.jpg",
+  //   title: "Confit de canard",
+  //   author: "Тушенка",
   // },
   // {
   //   img: "https://i.ibb.co/W5p6sCP/e35ecf41-2ef1-4eb6-beee-25c8ac239571.jpg",
